a921d0326bc9d0cf631afdb130f8513e18e659de
sujeet kushwaha <sujeetkushwaha2802gmail.com>
2024-06-27 00:56:09 +0530

    add one more api calling

--- api_calling.js
+++ api_calling.js
@@ -3,3 +3,11 @@ const fetchData = async()=>{
     const result = await responce.json()
     console.log(result);
 }
+fetchData();
+
+const fetchData1 = async()=>{
+    const responce = fetch("https://jsonplaceholder.typicode.com/users");
+    const result = await responce.json()
+    console.log(result);
+}
+fetchData();
